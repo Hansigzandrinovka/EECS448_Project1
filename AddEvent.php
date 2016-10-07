7ba638cc6b05b8400df052fd691f7ef593aa01d3
dshoaibi <dshoaibi@yahoo.com>
2016-10-06 22:35:50 -0500

    Create AddEvent.php

---
+++ AddEvent.php
@@ -0,0 +1,14 @@
+<html>
+  <head></head>
+
+  <body>
+    <h2>Add Event</h2>
+    <Form name="addEvent" method="post" onsubmit="" actoin="validateEvent.php">
+    <p>Title:</p><input type="text" name="title" id="title" ><br>
+    Date:<input type="date"><br>
+    Time: From<timepicker value="12:05"/> tO<timepicker value="12:05"/>
+  
+    </form>
+  </body>
+
+</html>
